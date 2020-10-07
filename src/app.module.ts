0762cf04a1014ab3fb82059974c4f0a4a31a9a88
TarasWebdew <47381043+Ezen1programmer@users.noreply.github.com>
2020-10-07 21:52:56 +0300

    User CRUD operations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,24 @@
-import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
-import {ConfigModule} from '@nestjs/config';
-import {TypeOrmModule} from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
-    imports: [ConfigModule.forRoot({
-        isGlobal: true
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
-        TypeOrmModule.forRoot({
-            type: 'postgres',
-            url: process.env.DATABASE_URL,
-            autoLoadEntities: true,
-            synchronize: true,
-        }),],
-    controllers: [AppController],
-    providers: [AppService],
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
